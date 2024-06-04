b5d70e90f3ce9bc0766db0ea6a5a805fc491deac
santiago <arleycubillos20@gmail.com>
2024-06-03 21:14:16 -0400

    add new elements

--- bin/app/dashboard.py
+++ bin/app/dashboard.py
@@ -1,17 +1,17 @@
 import time
-from dotenv import load_dotenv
 import os
 import simplejson as json
-import random
+from kafka import KafkaConsumer
 
 import streamlit as st
 import psycopg2
 import pandas as pd
 import plotly.express as px
-from kafka import KafkaConsumer
 import matplotlib.pyplot as plt
 
 # Load environment variables from .env file
+from dotenv import load_dotenv
+
 load_dotenv()
 
 host = os.environ.get('HOST')
@@ -68,10 +68,37 @@ def fetch_data_from_kafka(consumer):
     return data
 
 
+def plot_header(total_events, total_customers):
+    col1, col2 = st.columns(2)
+    col1.metric('Total events', total_events)
+    col2.metric('Total customers', total_customers)
+
+
+def plot_most_loved_products(most_loved_products):
+    columns = ['title', 'image', 'category', 'count_product', 'rank']
+    df = pd.DataFrame(most_loved_products, columns=columns)
+    best_product = df[df['rank'] == 1].iloc[0]
+
+    # Display the best-ranked product details
+    col1, col2 = st.columns([1, 2])
+    with col1:
+        st.image(best_product['image'], width=200)
+    with col2:
+        st.header(best_product['title'])
+        st.subheader(f"Category: {best_product['category']}")
+        st.subheader(f"Clicks in the product: {best_product['count_product']}")
+
+
 def plot_barplot_chart(df_data: pd.DataFrame):
     plt.bar(df_data['city'], df_data['count'])
     plt.tight_layout()
     return plt
+
+
+def plot_effectiveness_chart():
+    df_data = fetch_effect_stats()
+    st.header("Effectiveness of the Top 10 campaigns")
+    st.table(df_data)
 
 
 @st.cache_data
@@ -85,45 +112,52 @@ def fetch_campaign_city_stats():
               sort_values(by='Total events', ascending=False).head(10))
     return df_res
 
+@st.cache_data
+def fetch_effect_stats() -> pd.DataFrame:
+    consumer = create_kafka_consumer("campaign-effectiveness")
+    data = fetch_data_from_kafka(consumer)
+    df_data = pd.DataFrame(data)
+    df_effect = (df_data.
+                 groupby(['campaign_id', 'type_event'])['total_events'].
+                 count().
+                 reset_index().
+                 fillna(0).
+                 pivot(index='campaign_id', columns='type_event', values='total_events'))
+
+    df_effect['effectiveness'] = df_effect.apply(
+        lambda row: (row['CLICKED'] / row['OPENED']) * 100
+        if row['CLICKED'] <= row['OPENED'] else 100, axis=1)
+
+    return df_effect.sort_values(by=['CLICKED', 'OPENED', 'effectiveness'], ascending=False).head(10)
+
 
 def update_data():
     last_refresh = st.empty()
     last_refresh.text(f'Last refresh at: {time.strftime("%Y-%m-%d %H:%M:%S")}')
 
-    # Fetch events statistics from PostgreSQL
+    # Fetch events statistics from PostgresSQL
     total_events, total_customers, most_loved_products = fetch_events_stats()
 
     # Display metrics in two columns
     st.markdown("""---""")
-    col1, col2 = st.columns(2)
-    col1.metric('Total events', total_events)
-    col2.metric('Total customers', total_customers)
+    plot_header(total_events, total_customers)
+    plot_effectiveness_chart()
+
     # Display best-ranked product
     st.markdown("""---""")
     st.title('Best Ranked Product')
+    plot_most_loved_products(most_loved_products)
 
-    # Create a DataFrame for the most loved products
-    columns = ['title', 'image', 'category', 'count_product', 'rank']
-    df = pd.DataFrame(most_loved_products, columns=columns)
-    best_product = df[df['rank'] == 1].iloc[0]
-
-    # Display the best-ranked product details
-    col1, col2 = st.columns([1, 2])
-    with col1:
-        st.image(best_product['image'], width=200)
-    with col2:
-        st.header(best_product['title'])
-        st.subheader(f"Category: {best_product['category']}")
-        st.subheader(f"Total Count: {best_product['count_product']}")
-
-        # Display data from Kafka
-        st.markdown("""---""")
-        st.subheader('Cities with most events traffic')
-        col1_city, col2_city = st.columns(2)
-        with col1_city:
-            df_city = fetch_campaign_city_stats()
-            fig = px.bar(df_city, x='city', y='Total events', color=df_city['city'])
-            st.plotly_chart(fig)
+    st.markdown("""---""")
+    st.subheader('Cities with most events traffic')
+    col1_city, col2_city = st.columns(2)
+    df_city = fetch_campaign_city_stats()
+    with col1_city:
+        fig = px.bar(df_city, x='city', y='Total events', color=df_city['city'])
+        st.plotly_chart(fig)
+        st.dataframe(df_city)
+    with col2_city:
+        pass
 
 
 if __name__ == '__main__':
